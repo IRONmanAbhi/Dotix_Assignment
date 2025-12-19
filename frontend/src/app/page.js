cd5c8baa9bdf3f7d6561574987a4c4f4b58c8bbf
IRONmanAbhi <ironmanabhinav1@gmail.com>
2025-12-19 20:18:35 +0530

    Updates
    Containerised the application

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
-const API = "http://localhost:5000";
+const API = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Dashboard() {
   const [jobs, setJobs] = useState([]);
